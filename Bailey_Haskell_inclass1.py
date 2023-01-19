ca640362dc2c9b34a35045b1e07727b19fffdea6
Bailey Haskell <Bailey Haskell>
2023-01-19 11:56:25 -0700

    corrected to catch string exception

--- Bailey_Haskell_inclass1.py
+++ Bailey_Haskell_inclass1.py
@@ -1,5 +1,10 @@
-val = float (input("Enter a dividend: "))
-val2 = float (input("Enter a divisor: "))
-total = val / val2
-print(total)
+try:
+    val = float (input("Enter a dividend: "))
+    val2 = float (input("Enter a divisor: "))
+    total = val / val2
+    print(total)
+    
+except ValueError:
+    print("Please enter a number")
+
 
