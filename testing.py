449d0a3531e14b7b6ce62fe6bae0d7e0762c94ae
Bailey Haskell <Bailey Haskell>
2023-02-02 17:01:02 -0700

    testing

--- testing.py
+++ testing.py
@@ -1,6 +1,16 @@
-total = 0
-while True:
-  print("hi")
-  total = total +1
-  if total > 100:
-    break
+for row in range(8):
+  if row % 2 == 0:
+    for column in range(8):
+      if column % 2 == 0:
+        print("X", end='')
+      else:
+        print("O", end='')
+    print()
+  else:
+    for column in range(8):
+      if column % 2 == 0:
+        print("O", end='')
+      else:
+        print("X", end='')
+    print()
+
