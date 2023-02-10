9c7fefe1984eeac6aa03367d4f1012f278425a7b
HaskizMe <haskellb2017@gmail.com>
2023-02-09 17:29:35 -0700

    Homework

--- testing.py
+++ testing.py
@@ -1,6 +1,40 @@
-total = 0
-while True:
-  print("hi")
-  total = total +1
-  if total > 100:
-    break
+# ########################################################
+# # DO NOT EDIT THE CODE IN THE SECTION BELOW
+# ########################################################
+# import sys
+
+# numbers = sys.argv[1:]
+# for i in range(len(numbers)):
+#   numbers[i] = int(numbers[i])
+# ########################################################
+# # Enter your code below
+# ########################################################
+# for i in range(len(numbers)):
+#   if(numbers[i]>10):
+#     numbers[i] = '*'
+
+# print(numbers)
+
+# problem 2
+########################################################
+# DO NOT EDIT THE CODE IN THE SECTION BELOW
+########################################################
+# import sys
+
+# my_list = sys.argv[1:]
+
+# ########################################################
+# # Enter your code below
+# ########################################################
+# if len(my_list) < 5:
+#   print(my_list * 3)
+# else:
+#   print(my_list)
+
+# problem 3
+import sys
+
+strings = sys.argv[1:]
+
+sorted(strings)
+print(strings[0])
