f28779dfdd32efe6cc472a66a4bf592c2e2ca056
Bailey Haskell <Bailey Haskell>
2023-03-02 11:08:07 -0700

    test

--- testing.py
+++ testing.py
@@ -1,88 +1,24 @@
-# # ########################################################
-# # # DO NOT EDIT THE CODE IN THE SECTION BELOW
-# # ########################################################
-# # import sys
-
-# # numbers = sys.argv[1:]
-# # for i in range(len(numbers)):
-# #   numbers[i] = int(numbers[i])
-# # ########################################################
-# # # Enter your code below
-# # ########################################################
-# # for i in range(len(numbers)):
-# #   if(numbers[i]>10):
-# #     numbers[i] = '*'
-
-# # print(numbers)
-
-# # problem 2
-# ########################################################
-# # DO NOT EDIT THE CODE IN THE SECTION BELOW
-# ########################################################
-# # import sys
-
-# # my_list = sys.argv[1:]
-
-# # ########################################################
-# # # Enter your code below
-# # ########################################################
-# # if len(my_list) < 5:
-# #   print(my_list * 3)
-# # else:
-# #   print(my_list)
-
-# # problem 3
-# # import sys
-
-# # strings = sys.argv[1:]
-
-# # strings.sort()
-# # print(strings[0])
-
-# # # problem 5
-# # import sys
-
-# # numbers = sys.argv[1:]
-# # for i in range(len(numbers)):
-# #   numbers[i] = int(numbers[i])
-
-# # diff = numbers[1] - numbers[0]
-# # numbers.append(numbers[len(numbers)-1]+diff)
-# # numbers.append(numbers[len(numbers)-1]+diff)
-# # print(numbers)
-
-# # problem 6
-# # import sys
-
-# # number = int(sys.argv[1])
-# # data = [[0 for i in range(number)] for j in range(number)]
-
-# # for row in range(number):
-# #   for column in range(number):
-# #     if row == column:
-# #       data[row][column] = 1
-# #     print(f"{data[row][column]} ", end="")
-# #   print()
-# ########################################################
-# # DO NOT EDIT THE CODE IN THE SECTION BELOW
-# ########################################################
 # import sys
 
-# numbers = sys.argv[1:]
-# for i in range(len(numbers)):
-#   numbers[i] = int(numbers[i])
+# test_file = sys.argv[1]
 
-# ########################################################
-# # Enter your code below
-# ########################################################
+# line_count = 0
+# char_count = 0
 
-# for i in range(len(numbers)):
-#   if(numbers[i]%2 ==0):
-#     numbers[i] = "even"
-#   else:
-#     numbers[i] = "odd"
-# print(numbers)
-x = 5
+# with open(test_file, "r") as input_file:
+#     line = input_file.readline()
+#     while line != "":
+#         line_count += 1
+#         char_count += len(line)
+#         line = input_file.readline()
 
-if(x == 4 or x == 5):
-    print("true")
+# print("{} lines".format(line_count))
+# print("{} characters".format(char_count))
+
+import sys
+
+results = []
+test_file = sys.argv[1]
+with open(test_file, "r") as input_file:
+    for row in input_file:
+        results.append(row)
