4593925ee0c835cfc0e514e73545c99eade69b0a
HaskizMe <haskellb2017@gmail.com>
2023-03-23 18:41:26 -0600

    working on library of congress

--- library_of_congress/library.py
+++ library_of_congress/library.py
@@ -1,6 +1,28 @@
+import sys
 
+def read_file(file):
+    count = 0
+    with open(file, 'r',encoding='utf-8') as filedata:
+        for line in filedata:
+            # print(line.rstrip())
+            x = len(filedata.readlines())
+            if("TTL" in line):
+                print(line)
+                count =+ 1
+            if("ALG" in line):
+                print(line)
+                count =+ 1
+            if("WOO" in line):
+                print(line)
+                count = count + 1
+    print(x)
+            
+
+    # print(line_list)
 def main():
-    print("hey")
+    input_file = sys.argv[1]
+    read_file(input_file)
+
 
 
 if __name__ == "__main__":
