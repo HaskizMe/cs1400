61f71549ff62e72d1c3ce31eabd33c0bc720d22e
HaskizMe <haskellb2017@gmail.com>
2023-04-18 16:43:14 -0600

    testing

--- testing.py
+++ testing.py
@@ -93,145 +93,67 @@
 
 
 
-<<<<<<< HEAD
-# # 1
-# def find_key(d, v):
-#   keys = list(d.keys())
-#   values = list(d.values())
-#   index = values.index(v)
-#   return keys[index]
-
-# #2
-
-# def move_to_bottom(d, k):
-#   if k not in d:
-#     return 'The key is not in the dictionary'
-#   else:
-#     value = d.pop(k)
-#     d[k] = value
-#     return d
-
-# #3
-# def swap(d):
-#   keys = d.keys()
-#   values = d.values()
-#   swapped_tuples = zip(values, keys)
-#   value_types = [type(elem) for elem in values]
-  
-#   if type({}) in value_types or type([]) in value_types:
-#     return 'Cannot swap the keys and values for this dictionary'
-#   else:
-#     new_dict = dict(swapped_tuples)
-#     return new_dict
-
-# #4
-# def is_nested(d):
-#   values = d.values()
-#   value_types = [type(elem) for elem in values]
-#   if type(()) in value_types or type([]) in value_types or type({}) in value_types:
-#     return True
-#   else:
-#     return False
-
-# #5
-# import json
-
-# def compare(f1, f2):
-#   with open(f1) as file1, open(f2) as file2:
-#     data1 = json.load(file1)
-#     data2 = json.load(file2)
-#     if data1 == data2:
-#       return 'The dictionaries are equal'
-#     else:
-#       count1 = len(data1)
-#       count2 = len(data2)
-#       if count1 > count2:
-#         return 'Dictionary 1 is longer than dictionary 2'
-#       elif count2 > count1:
-#         return 'Dictionary 2 is longer than dictionary 1'
-#       else:
-#         return 'Dictionary 1 and dictionary 2 have the same length'
-
-
-#1
-def key_position(d, k):
-  keys = list(d.keys())
-
-  if k in keys:
-    return keys.index(k)
-  else:
-    return 'Key not found'
-
-#2
-
-#3
-
-#4
-
-#5
-
-=======
 # lab 3 prompt 2
 # print("Menu Items Ranked In Order from Best to Worst:\n")
 # for counter_value, (menu_item, menu_item_rating) in enumerate(sorted(zip(menu_items, menu_item_ratings), key = lambda element: element[1], reverse = True), 1):
 #   print(counter_value, ":", menu_item, "with a rating of:", menu_item_rating, ".")
 
-string = "Mar  9 11:10:37 server spamdyke[29207]: DENIED_RDNS_MISSING from: spam@mail.spam-source.net to: recipient@example.com origin_ip: 203.0.113.66 origin_rdns: (unknown) auth: (unknown) encryption: (none) reason: (empty)"
-first = string.find('@')
-second = string.find('@', first+1)
-print(first)
-print(second)
+# string = "Mar  9 11:10:37 server spamdyke[29207]: DENIED_RDNS_MISSING from: spam@mail.spam-source.net to: recipient@example.com origin_ip: 203.0.113.66 origin_rdns: (unknown) auth: (unknown) encryption: (none) reason: (empty)"
+# first = string.find('@')
+# second = string.find('@', first+1)
+# print(first)
+# print(second)
 
-# print(len("Mar  9 11:10:37 server spamdyke[29207]: DENIED_RDNS_MISSING from: spam@mail.spam-source.net to: recipient@example.com origin_ip: 203.0.113.66 origin_rdns: (unknown) auth: (unknown) encryption: (none) reason: (empty)"))
-
-
-
-import sys
-import os.path
+# # print(len("Mar  9 11:10:37 server spamdyke[29207]: DENIED_RDNS_MISSING from: spam@mail.spam-source.net to: recipient@example.com origin_ip: 203.0.113.66 origin_rdns: (unknown) auth: (unknown) encryption: (none) reason: (empty)"))
 
 
-def find_email(line):
-    email = ""
-    for i in range(len(line)):
-        if(line[i] == '@'):
-            at_index = line.find('@')
-            if(line.count('@') > 1):
-                # print("second @")
-                at_index2 = line.find('@', at_index+1)
+
+# import sys
+# import os.path
+
+
+# def find_email(line):
+#     email = ""
+#     for i in range(len(line)):
+#         if(line[i] == '@'):
+#             at_index = line.find('@')
+#             if(line.count('@') > 1):
+#                 # print("second @")
+#                 at_index2 = line.find('@', at_index+1)
    
-                space2 = line.find(' ',at_index2)
-                space1 = line.rfind(' ',0,at_index2) + 1
-                # if(space2 == -1):
-                #     space2 = (len(line))-1
+#                 space2 = line.find(' ',at_index2)
+#                 space1 = line.rfind(' ',0,at_index2) + 1
+#                 # if(space2 == -1):
+#                 #     space2 = (len(line))-1
             
-                email = line[space1:space2]
-                # return email
-            space2 = line.find(' ',at_index)
-            space1 = line.rfind(' ',0,at_index) + 1
-            if(space2 == -1):
-                space2 = (len(line))-1
+#                 email = line[space1:space2]
+#                 # return email
+#             space2 = line.find(' ',at_index)
+#             space1 = line.rfind(' ',0,at_index) + 1
+#             if(space2 == -1):
+#                 space2 = (len(line))-1
             
-            email = line[space1:space2]
-            # return email
+#             email = line[space1:space2]
+#             # return email
         
-    return email
+#     return email
 
-def read_file(file):
-    result = []
-    with open(file, 'r') as my_file:
-        for line in my_file:
-            # find_email(line)
-            if(find_email(line) != ""):
-                result.append(find_email(line))
-    print_emails(result)
+# def read_file(file):
+#     result = []
+#     with open(file, 'r') as my_file:
+#         for line in my_file:
+#             # find_email(line)
+#             if(find_email(line) != ""):
+#                 result.append(find_email(line))
+#     print_emails(result)
 
-def print_emails(my_list):
-    for i in range (len(my_list)):
-        print(i+1,my_list[i])
-def main():
-    if (os.path.isfile(sys.argv[1])):
-        input_file = sys.argv[1]
-        read_file(input_file)
-if __name__ == "__main__":
-    main()
->>>>>>> 68558ff1676f4e63c23f96f68f9411f173e7988f
+# def print_emails(my_list):
+#     for i in range (len(my_list)):
+#         print(i+1,my_list[i])
+# def main():
+#     if (os.path.isfile(sys.argv[1])):
+#         input_file = sys.argv[1]
+#         read_file(input_file)
+# if __name__ == "__main__":
+#     main()
+print("hello world")
